10ddc1b31e4a45254900439f62860ee158422134
Abubakr Inogomov <abubbaakr13@gmail.com>
2024-08-05 15:04:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var usersName = prompt('Write down your name')
+console.log('My name is ' + usersName );
+
+var usersAge = +prompt('Write down your age' )
+console.log('I am ' + usersAge);
+
+
+var countfirst = +prompt('Write answer for 15 + 5 = ' )
+console.log('Count 1: ' + ('15 + 5 = ') + (15+5) + ' your answer is ' + countfirst)
+
+var countsecond = +prompt('Write answer for 15 - 5 = ' )
+console.log('Count 1: ' + ('15 - 5 = ') + (15-5) + ' your answer is ' + countsecond)
+
+var countthird = +prompt('Write answer for 15 * 5 = ' )
+console.log('Count 1: ' + ('15 * 5 = ') + 15*5 + ' your answer is ' + countthird)
+
+var countfourth = +prompt('Write answer for 15 / 5 = ' )
+console.log('Count 1: ' + ('15 / 5 = ') + 15/5 + ' your answer is ' + countfourth)
+
+var countfifth = +prompt('Write answer for 15 % 5 = ' )
+console.log('Count 1: ' + ('15 % 4 = ') + 15%4 + ' your answer is ' + countfifth)
+
+
+var alert = alert('Open console to view your answers ' + usersName)
+
